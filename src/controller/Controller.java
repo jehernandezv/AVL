a2822460ac3ec3aac2df0a1c6fc629f583b265f7
jhonn vega <jhonn.hernandez@uptc.edu.co>
2018-04-08 17:12:12 -0500

    Se crea el package controller con la clase Controller esta clase implementa Action Listener para la interface con el usuario, tambien atributos del modelo la clase Tree y de la vista la clase WindowTree, metodos: actionPerformed, validateRotation, en el constructor se instancia la vista y se envia el nodo principal para que sea pintada en la vista

---
+++ src/controller/Controller.java
@@ -0,0 +1,44 @@
+﻿package controller;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JOptionPane;
+
+import models.Node;
+import models.Tree;
+import views.WindowTree;
+
+public class Controller implements ActionListener{
+	
+	private Tree tree;
+	private WindowTree window;
+
+	public Controller() {		
+		tree = new Tree();
+		window = new WindowTree(this);
+		window.paintTree(tree.getRoot());
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if (e.getActionCommand().equals("ADD")) {
+			Node node = new Node(Integer.valueOf(JOptionPane.showInputDialog("id")));	
+			this.validateRotation(node);
+		}else if(e.getActionCommand().equals("DELETE")){
+			tree.delete(Integer.parseInt(JOptionPane.showInputDialog("Información del nodo a borrar")));
+		}
+		window.paintTree(tree.getRoot());
+	}
+	
+	public void validateRotation(Node node){
+		if(tree.getRoot() == null){
+			tree.add(node);
+		}else if(tree.getRoot().calculateBalance() < -1 || tree.getRoot().calculateBalance() > 1) {
+			tree.rotation(tree.getRoot());
+			tree.add(node);
+		}else if(tree.getRoot().calculateBalance() > -2 && tree.getRoot().calculateBalance() < 2){
+			tree.add(node);
+		}
+	}
+}
