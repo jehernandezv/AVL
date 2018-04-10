0d1a92174eadd7f71145bb249393e2e99529ef77
jhonn vega <jhonn.hernandez@uptc.edu.co>
2018-04-10 15:49:03 -0500

    Se elimino un boton innecesario en la clase WindowTree

--- src/views/WindowTree.java
+++ src/views/WindowTree.java
@@ -4,7 +4,6 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.event.ActionListener;
-
 import javax.swing.JButton;
 import javax.swing.JFrame;
 
@@ -29,11 +28,7 @@ public class WindowTree extends JFrame{
 		btnDelete.addActionListener(listener);
 		btnDelete.setActionCommand("DELETE");
 		add(btnDelete, BorderLayout.PAGE_END);
-		
-		JButton btnRotate = new JButton("---->");
-		btnRotate.addActionListener(listener);
-		btnRotate.setActionCommand("Rotate");
-		add(btnRotate, BorderLayout.EAST);
+
 		setVisible(true);
 	}
 
@@ -51,15 +46,19 @@ public class WindowTree extends JFrame{
 	}
 
 	public void addNode(Node node, int x, int width, int height, Graphics g) {
+		
 		if (node != null) {
+			
 			g.fillOval(x + width, height, 40, 40);
 			g.setColor(Color.WHITE);
 			g.drawString(node.toString(), x + width + 17, height + 23);
 			g.setColor(Color.BLACK);
 			g.drawString("FB: "+ String.valueOf(node.calculateBalance()), x + width, height);
 			g.setColor(Color.BLACK);
+			if(node.getLeft() != null){
 			addNode(node.getLeft(), x + width, -1 * (Math.abs(width) / 2), height + 50, g);
-			//g.drawLine(x + width, height,x + width , (Math.abs(width)/2) + height + 50);
+			
+			}
 			addNode(node.getRight(), x + width, (Math.abs(width) / 2), height + 50, g);
 		}
 	}
